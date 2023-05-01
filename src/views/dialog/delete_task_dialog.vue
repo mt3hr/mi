ceff505f05866a42976090668513fc80d535b5b3
Yamato Sanekata <yamatomrp305@gmail.com>
2023-05-02 08:18:52 +0900

    フロント側、delete_task_dialog画面の実装をしました。

---
+++ src/views/dialog/delete_task_dialog.vue
@@ -0,0 +1,76 @@
+<template>
+    <v-dialog v-model="is_show">
+        <v-card class="pa-5">
+            <v-card-title>
+                タスク削除
+            </v-card-title>
+            <v-card-text>{{ task_info.task_title_info.title }}</v-card-text>
+            <v-card-actions>
+                <v-row>
+                    <v-col cols="auto">
+                        <v-btn @click="close_dialog" tabindex="103">
+                            キャンセル
+                        </v-btn>
+                    </v-col>
+                    <v-spacer />
+                    <v-col cols="auto">
+                        <v-btn @click="delete_task" tabindex="102">
+                            削除
+                        </v-btn>
+                    </v-col>
+                </v-row>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script setup lang="ts">
+import DeleteTaskRequest from '@/api/DeleteTaskRequest';
+import MiServerAPI from '@/api/MiServerAPI';
+import TaskInfo from '@/api/data_struct/TaskInfo';
+import { Ref, ref, watch } from 'vue';
+
+interface Props {
+    show: boolean
+    task_info: TaskInfo
+}
+
+const props = defineProps<Props>()
+const emits = defineEmits<{
+    (e: 'errors', errors: Array<string>): void
+    (e: 'deleted_task'): void
+}>()
+
+let is_show: Ref<boolean> = ref(props.show)
+
+watch(() => props.show, () => {
+    is_show.value = props.show
+})
+
+function close_dialog() {
+    is_show.value = false
+}
+function delete_task() {
+    const api = new MiServerAPI()
+    const request = new DeleteTaskRequest()
+    request.task_id = props.task_info.task.task_id
+    api.delete_task(request)
+        .then(res => {
+            if (res.errors.length != 0) {
+                emit_errors(res.errors)
+                return
+            }
+            emit_deleted_task()
+            close_dialog()
+        })
+}
+
+function emit_errors(errors: Array<string>) {
+    emits("errors", errors)
+}
+function emit_deleted_task() {
+    emits("deleted_task")
+}
+</script>
+
+<style></style>
